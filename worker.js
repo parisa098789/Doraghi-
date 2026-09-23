4a1bde259e41d6651931e40af690c03ac968ed0a
parisa098789 <shyl44681@gmail.com>
2026-09-23 23:21:26 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,18 +2,16 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname.startsWith("/api/")) {
-      return new Response(
-        JSON.stringify({
-          ok: true,
-          message: "Doraghi API is working"
-        }),
-        {
-          headers: {
-            "content-type": "application/json; charset=UTF-8"
-          }
+    if (url.pathname === "/api/products" && request.method === "GET") {
+      const result = await env.DB
+        .prepare("SELECT * FROM products ORDER BY id DESC")
+        .all();
+
+      return new Response(JSON.stringify(result.results), {
+        headers: {
+          "content-type": "application/json; charset=UTF-8"
         }
-      );
+      });
     }
 
     return env.ASSETS.fetch(request);
